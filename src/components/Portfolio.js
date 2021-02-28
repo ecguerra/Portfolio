212c3b4e0501db88bc9b3471ddd3728b23097b79
Elizabeth Guerra <ecguerra@outlook.com>
2021-02-27 19:04:00 -0500

    linked portfolio projects

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,7 +1,29 @@
 const Portfolio = () => {
     return(
         <div>
-            Portfolio
+            <h1>Portfolio</h1>
+            <div>
+                <h2>Cora Breaks Everything!</h2>
+                <a target='_blank' href='https://ecguerra.github.io/Bruno-ElizabethGuerra_P1/index.html'>The Site</a> {' '}
+                <a target='_blank' href='https://github.com/ecguerra/Bruno-ElizabethGuerra_P1'>The Code</a>
+            </div>
+            <div>
+                <h2>Decloset</h2>
+                <a target='_blank' href='https://decloset.herokuapp.com/'>The Site</a>{' '}
+                <a target='_blank' href='https://github.com/ecguerra/Project_2_GA'>The Code</a>
+            </div>
+            <div>
+                <h2>COV-ID</h2>
+                <a target='_blank' href='https://cov-id.netlify.app/'>The Site</a> {' '}
+                <a target='_blank' href='https://github.com/ecguerra/team-6-frontend'>The Code (Frontend)</a>{' '}
+                <a target='_blank' href='https://github.com/ecguerra/Team-6-backend'>The Code (Backend)</a> 
+            </div>
+            <div>
+                <h2>Palette Town</h2>
+                <a target='_blank' href='https://palettetown.netlify.app/'>The Site</a> {' '}
+                <a target='_blank' href='https://github.com/ecguerra/Project_4_frontend'>The Code (Frontend)</a> {' '}
+                <a target='_blank' href='https://github.com/ecguerra/Project_4_backend'>The Code (Backend)</a> {' '}
+            </div>
         </div>
     )
 }
