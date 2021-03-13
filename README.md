415e63876ae1bb5eabbac22cc066fdb8bae10ae5
Elizabeth Guerra <ecguerra@outlook.com>
2021-03-13 18:33:47 -0500

    navbar is responsive

--- README.md
+++ README.md
@@ -5,3 +5,6 @@
 #### Photo credit (Homepage): Me, taken at the ICA in Boston, MA
 
 ## Mobile/responsive site in progress!
+
+## Acknowledgements
+* https://sdever.medium.com/building-a-responsive-navigation-bar-with-react-css-d9b30af03c20 for responsive help
--- src/components/common/Layout.js
+++ src/components/common/Layout.js
@@ -1,18 +1,31 @@
+import { useState } from 'react'
 import { Link } from 'react-router-dom'
+import { FiMenu } from 'react-icons/fi'
 
 import '../../css/Layout.css'
 
 const Layout = props => {
+    const [click, setClick] = useState(false)
+    const handleClick = () => setClick(!click)
+    const closeMobileMenu = () => setClick(false)
+
     return(
         <>
             <nav>
                 <div className='logo'>
                     <Link to='/'>Elizabeth Guerra</Link>{' '}
                 </div>
-                <div className='nav-link'>
-                    <Link to='/about'>About Me</Link>{' '}
-                    <Link to='/portfolio'>Portfolio</Link>{' '}
-                    <Link to='/contact'>Contact</Link>{' '}
+                <div className={click ? 'nav-link active' : 'nav-link'}>
+                        <Link to='/about' onClick={closeMobileMenu}>About Me</Link>{' '}
+                        <Link to='/portfolio' className='nav-choice' onClick={closeMobileMenu}>Portfolio</Link>{' '}
+                        <Link to='/contact' className='nav-choice' onClick={closeMobileMenu}>Contact</Link>{' '}
+                </div>
+                <div className='mobile-menu' onClick={handleClick}>
+                    {click ? (
+                        <FiMenu className='menu-icon open' />
+                    ) : (
+                        <FiMenu className='menu-icon closed' />
+                    )}
                 </div>
             </nav>
             <div>
